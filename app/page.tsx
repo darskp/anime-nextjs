47a9d0f4d0e50a3111fcb8c931c472554f10e3dc
darskp <darshanpatgar100@gmail.com>
2024-04-26 00:55:31 +0530

    anime card design

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import AnimeCard, { AnimeProp } from "@/components/AnimeCard";
+import { data } from "./_data";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
+    <main className="flex flex-col gap-10 px-8 py-16 sm:p-16">
+      <h2 className="font-bold text-white text-3xl">Explore Anime</h2>
+      <section className="grid grid-cols-1 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 gap-10">
+        {data.map((item: AnimeProp, index) => (
+          <AnimeCard key={item.id} anime={item} index={index} />
+        ))}
+      </section>
     </main>
   );
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,42 @@
+import Image from 'next/image'
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div>Footer</div>
-  )
+    return (
+        <footer className='flex-wrap py-4 px-8 sm:px-16 flex-between gap-2 w-full bg-[#161921]'>
+            <p className='text-white font-bold text-base'>@2023 EpicAnimeVault</p>
+            <Image
+                src="./logo.svg"
+                alt="logo"
+                width={47}
+                height={44}
+                className="object-contain"
+            />
+            <div className='flex-between gap-5 '>
+                <Image
+                    src="./instagram.svg"
+                    alt="logo"
+                    width={19}
+                    height={19}
+                    className="object-contain"
+                />
+                <Image
+                    src="./tiktok.svg"
+                    alt="logo"
+                    width={19}
+                    height={19}
+                    className="object-contain"
+                />
+                <Image
+                    src="./twitter.svg"
+                    alt="logo"
+                    width={19}
+                    height={19}
+                    className="object-contain"
+                />
+            </div>
+        </footer>
+    )
 }
 
 export default Footer
